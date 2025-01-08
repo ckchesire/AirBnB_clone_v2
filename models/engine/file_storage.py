4c837a12f7d65ffea55db01877c007064e52007f
ckchesire <christianck.chesire@gmail.com>
2025-01-08 22:25:36 +0300

    Update FileStorage method to delete object and method that returns the list of objects of one type of class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage
+
+        Args:
+            cls(class, optional): Class to filter by, include only
+            objects of the specified class.
+        
+        Returns:
+            Dictionary of objects of specified class, or all objects if
+            class is unspecified
+        """
+        if cls:
+            if isinstance(cls, str):
+                cls = globals().get(cls)
+            if cls and issubclass(cls, BaseModel):
+                my_dict = {k: v for k,
+                           v in self.__objects.items() if isinstance(v, cls)}
+                return my_dict
         return FileStorage.__objects
 
     def new(self, obj):
@@ -27,14 +49,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
@@ -48,3 +62,20 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside
+
+        Args:
+            obj(optional): Object to delete
+        """
+        if obj is None:
+            return
+        del_obj = obj.__class__.__name__ + '.' + obj.id
+        try:
+            del FileStorage.__objects[del_obj]
+        except AttributeError:
+            pass
+        except KeyboardInterrupt:
+            pass
+
