8e493dc7a016892c76557e42cbd19012a83ae6ae
ckchesire <christianck.chesire@gmail.com>
2025-01-08 17:27:51 -0800

    Resolved city class bug by including Base parameter to inherit.

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import os
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, Base
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
@@ -12,9 +12,9 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') ==  'db':
-        cities = relationship('City', 
-                              backref='state', 
-                              cascade='all, delete-orphan')
+        cities = relationship("City", 
+                              backref="state", 
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
