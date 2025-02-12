f15f90466ad7f3a87038fb8de5c174bcfbf732f1
ckchesire <christianck.chesire@gmail.com>
2025-02-12 21:15:20 +0300

    5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module used to start a Flask web application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """Added function to root route, that displays greetings
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that displays "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_post(text):
+    """Function takes input parameter and renders it to user
+    """
+    formatted_text = text.replace('_', ' ')
+    return "C {}".format(formatted_text)
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_post(text):
+    """Function takes inpute parameter and renders it to user or displays
+       defualt text
+    """
+    formatted_text = text.replace('_', ' ')
+    return "Python {}".format(formatted_text)
+
+
+@app.route('/number/<int:n>')
+def show_number(n):
+    """Function to handle integer variables, passed as input and render it
+    """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Function that takes an integer as an input parameter and renders it
+       using 5-number.html
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
