0a61b3e5e3a725c1d4b439d32be7f83439eeb819
ckchesire <christianck.chesire@gmail.com>
2025-02-12 18:24:46 +0300

    Implemented route that sets default text if no <text> is provided.

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module used to start a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    """Added function to root route, that displays greetings
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Function that displays "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_post(text):
+    """Function takes input parameter and renders it to user
+    """
+    formatted_text = text.replace('_', ' ')
+    return "C {}".format(formatted_text)
+
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_post(text):
+    """Function takes inpute parameter and renders it to user or displays
+       defualt text
+    """
+    formatted_text = text.replace('_', ' ')
+    return "Python {}".format(formatted_text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
